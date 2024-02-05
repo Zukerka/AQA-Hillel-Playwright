a0b55593f2ffac24953e3ddcae012810528140f5
Natalia Kolesnychenko <n.kolesnychenko@gmail.com>
2024-02-05 18:30:25 +0200

    installed ts dependency and modified upload photo test

--- tests/profile.spec.ts
+++ tests/profile.spec.ts
@@ -6,9 +6,11 @@ test.describe('Update Profile tests', () => {
     test.only('Update profile avatar', async ({page}) => {
         const profilePage = new ProfilePage(page)
     
-        await page.goto('/panel/profile')
+        // await page.goto('/panel/profile')
+        await profilePage.open()
         await profilePage.editProfileBtn.click()
-        await page.setInputFiles(profilePage.editPhoto, 'fixtures/mario.png')
+        // await page.setInputFiles(profilePage.editPhoto, 'fixtures/mario.png')
+        await profilePage.setInputFiles('fixtures/mario.png')
         await profilePage.saveBtn.click()
         await expect(profilePage.successSnackBar).toBeVisible()
 
